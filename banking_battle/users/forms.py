a1cdd21c59fc09a99381ef098e6132d34a504dbe
Daniil <pimanov.d@gmail.com>
2022-08-11 16:08:29 +0300

    email is now required

--- banking_battle/users/forms.py
+++ banking_battle/users/forms.py
@@ -5,6 +5,12 @@ User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CreationForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].required = True
+
     class Meta():
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
