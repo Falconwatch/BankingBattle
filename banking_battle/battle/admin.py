4e18dda1f5498be3ad96a56d4e4a3fc1adc17bdd
Falconwatch <garrikwower@gmail.com>
2022-08-11 15:41:23 +0300

    Привязка пользователя к играм и командам

--- banking_battle/battle/admin.py
+++ banking_battle/battle/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Game, Team
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Team)
--- banking_battle/battle/models.py
+++ banking_battle/battle/models.py
@@ -2,3 +2,34 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=3000)
+    id = models.BigAutoField(primary_key=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=256)
+    id = models.BigAutoField(primary_key=True)
+    users_in_team = models.ManyToManyField(User, related_name='users_teams')
+    game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
