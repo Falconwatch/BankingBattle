5c20689c64089b73384fd2c5188573f0b0378315
Daniil <pimanov.d@gmail.com>
2022-08-31 09:50:07 +0300

    added migrations

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # start-server.sh
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd banking_battle; python manage.py createsuperuser --no-input)
 echo
 echo "CREATED SUPERUSER"
 echo
 fi
-(cd banking_battle; gunicorn banking_battle.wsgi --bind 0.0.0.0:8010 --workers 3)
+cd banking_battle
+python manage.py makemigrations
+python manage.py migrate
+(gunicorn banking_battle.wsgi --bind 0.0.0.0:8010 --workers 3)
